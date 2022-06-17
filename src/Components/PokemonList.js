bd11486db75caf0f6a55af88ffd1c5ec6949cdb5
githubsupercode <alex.supercode@gmail.com>
2022-06-17 17:16:57 +0200

    commit on 'main'

--- src/Components/PokemonList.js
+++ src/Components/PokemonList.js
@@ -1,28 +1,41 @@
 import { useState, useEffect } from 'react';
 import PokemonCard from './PokemonCard';
+import Axios from 'axios';
 
 const PokemonList = () => {
 
-    const [pokemon, setPokemon] = useState([]);
+    const [pokemon, setPokemon] = useState("");
 
-    useEffect(() => {
-        fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=151/")
-            .then(res => res.json())
-            .then(json => {
-                // setNews(json);
-                setPokemon(json.results);
-            })
-    }, [])
+    const [showData, setShowData] = useState({
+        name: "",
+    });
 
-    console.log(pokemon);
+    const searchPokemon = () => {
+        Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`).then(response => {
+            console.log(response.data);
+            setShowData({
+                name: pokemon,
+                img: response.data.sprites.other.dream_world.front_default,
+            });
+        })
+    }
+
+    // useEffect(() => {
+    //     fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+    //         .then(res => res.json())
+    //         .then(json => {
+    //             // setNews(json);
+    //             setPokemon(json.name);
+    //         })
+    // }, [])
+
+    // console.log(pokemon);
 
     return (<section>
-        {pokemon.map((elt, i) =>
-            <PokemonCard
-                key={i}
-                name={elt.name}
-            />
-        )}
+        <input type="text" name="" id="" onChange={(event) => setPokemon(event.target.value)} />
+        <button onClick={searchPokemon}>Search</button>
+        <p>{showData.name}</p>
+        <img src={showData.img} alt="" />
     </section>);
 }
 
